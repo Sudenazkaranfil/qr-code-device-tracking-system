dec07a71cb9826209db4bf6c3f74665ee7acb5b1
Sudenaz Karanfil <114330977+Sudenazkaranfil@users.noreply.github.com>
2025-05-13 00:31:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import psycopg2
 from datetime import datetime
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .env dosyasını yükler
 
 app = Flask(__name__)
 
