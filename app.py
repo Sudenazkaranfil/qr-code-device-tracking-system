28ffaef0a9240fc4a47b86105a4802e1987d54b8
Sudenaz Karanfil <114330977+Sudenazkaranfil@users.noreply.github.com>
2025-05-20 19:11:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,13 +10,7 @@ app = Flask(__name__)
 
 # Veritabanı bağlantısı
 def get_connection():
-    return psycopg2.connect(
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT")
-    )
+    return psycopg2.connect(os.getenv("DB_URL"), sslmode="require")
     
 @app.route("/")
 def home():
